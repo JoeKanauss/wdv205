576e271249700ac24f2622d9c10cde797fd06342
JoeKanauss <33582866+JoeKanauss@users.noreply.github.com>
2018-01-28 10:04:10 -0600

    wdv205 coursework

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../topBarStyle.css">
+<style>
+#twoOhFive{
+	background-color: black;
+	color: #e4018c;
+	font-weight: bold;
+	border: none;
+}
+</style>
+</head>
+<body>
+<?php include '../topBar.php';?>
+<div class="contain">
+<h1 class="titlePink">WDV 205: Advanced CSS</h1>
+
+<h2>Homework Files and Final Project</h2>	
+	
+ <p><a href="cssterms.html">Css Terms</a></p>
+ <p><a href="css3features.html">Css3 Features</a></p>
+ <p><a href="css3selectors.html">Css3 Selectors</a></p>
+ <p><a href="csspp.html">Css Pre-Processors</a></p>
+ <p><a href="downloadsass.html">Download and Implement SASS</a></p>
+ <p><a href="ppex.html">Pre-Processor Example</a></p> 
+ <p><a href="2d3dtransform.html">Research 2d and 3d Transformations</a></p>
+ <p><a href="2dTransformEx.html">Demonstrate Css3 2d Transformations</a></p>
+ <p><a href="3dTransformEx.html">Demonstrate Css3 3d Transformations</a></p>
+ <p><a href="transformMixIn.html">Demonstrate SASS Transformation Mixin</a></p>
+ <p><a href="transitionEx.html">Demonstrate Css3 Transition</a></p>
+ <p><a href="sassTrans.html">Demonstrate SASS Transition Mixin</a></p>
+ <p><a href="2dTransformTransition.html">Demonstrate 2d Transformation with a Transition</a></p>
+ <p><a href="researchAnimations.html">Research Css3 Animations</a></p>
+ <p><a href="demonstrateAnimations.html">Demonstrate Css3 Animations</a></p>
+ <p><a href="animationMixinEx.html">Demonstrate SASS Animations</a></p>
+ <p><a href="AdvertisingEffect.html">Advertising Effect</a></p>
+ <p><a href="mediaQuery.html">Media Queries/Display Property/Table</a></p>
+ <p><a href="researchBootstrap.html">Research and Review Bootstrap</a></p>
+ <p><a href="bootstrapEX.html">Demonstrate Bootstrap Features</a></p>
+ <p><a href="Bootstrap Project/bpHome.html">Bootstrap Application Project</a></p>
+ <p><a href="rdfResearch.html">Responsive Design Framework Research</a></p>
+ <p><a href="foundation.html">Responsive Design Framework Feature (Foundation)</a></p>
+ <p><a href="Foundation Framework Project/ffpHome.html">Responsive Design Framework Application (Foundation)</a></p>
+ <p><a href="foldoutMenu.html">Navigation Menu</a></p>
+ <p></p>
+ <h3><a href="205PortfolioProject/portfolioProject.html">FINAL - wdv205 Portfolio Project</a></h3>
+</div>
+</body>
+</html>
